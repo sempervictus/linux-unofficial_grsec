d4eb4d25f427aee0af8acd11a96649fd304dfbf7
RageLtMan <rageltman [at] sempervictus>
2017-10-11 03:28:16 -0400

    SCST ib_srpt const kernel_param

--- drivers/scst/srpt/ib_srpt.c
+++ drivers/scst/srpt/ib_srpt.c
@@ -129,7 +129,7 @@ MODULE_PARM_DESC(use_node_guid_in_target_name,
 
 static struct rdma_cm_id *rdma_cm_id;
 
-static int srpt_get_u64_x(char *buffer, struct kernel_param *kp)
+static int srpt_get_u64_x(char *buffer, const struct kernel_param *kp)
 {
 	return sprintf(buffer, "0x%016llx", *(u64 *)kp->arg);
 }
